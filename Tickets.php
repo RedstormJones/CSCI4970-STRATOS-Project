df0688950cedd1d8186deb1c381e7a555a4e72e9
Yumarzi Khan <ymkhan@unomaha.edu>
2015-03-13 18:13:22 -0500

    New Changes
    Added a meu and header php for reuse code
    Tickets page has a button to add ticket
    Add ticket form takes values from database to show in dropdown menu.
    Style css has more styles for website.

--- Tickets.php
+++ Tickets.php
@@ -1,75 +1,52 @@
 <html lang="en"> <!--<![endif]-->
-<head>
-<meta charset="utf-8">
-<title>Tickets</title>
-<link rel="stylesheet" type="text/css" href="style.css"> 
-</head>
+    <head>
+        <meta charset="utf-8">
+        <title>Tickets</title>
+        <link rel="stylesheet" type="text/css" href="style.css"> 
+    </head>
 
-<body>
-    <img src="Header.jpg" alt="Header Image" height="80" style="width:100%">
-
-    <div id='cssmenu'>
-
-        <ul class="menu">
-            <li>
-                <a href='Welcome.php'><span>Home</span></a>
-            </li>
-            <li>
-                <a href="Tickets.php">Tickets</a>
-            </li>
-            <li class="has-sub"><a href=""><span>Inventory</span></a>
-                <ul> 
-                    <li><a href=""><span>Hardware</span></a></li>
-                    <li><a href=""><span>Software</span></a></li>
-                </ul>
-            </li>
-            <li class="has-sub"><a href=""><span>Analytics</span></a>
-                <ul> 
-                    <li><a href=""><span>Metrics</span></a></li>
-                    <li><a href=""><span>Configuration</span></a></li>
-                </ul>
-            </li>
-            <li class="has-sub"><a href=""><span>Accounts</span></a>
-                <ul> 
-                    <li><a href=""><span>Account Settings</span></a></li>
-                    <li><a href=""><span>Logout</span></a></li>
-
-                </ul>
-            </li>
-    </div>
-    <br>
-    <br>
-    <table>
-        <tr>
-            <th>Checkbox</th>
-            <th>Ticket#</th>
-            <th>Tickets</th>		
-            <th>Subjects</th>
-            <th>Priority</th>
-            <th>Status</th>		
-            <th>Created</th>
-            <th>Last Updated</th>
-        </tr>
-        <tr>
-            <td></td>
-            <td>1234</td>
-            <td>Ticket1</td>		
-            <td>Subject1</td>
-            <td>Low</td>
-            <td>New</td>		
-            <td>Date</td>
-            <td>Date</td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>5678</td>
-            <td>Ticket2</td>		
-            <td>Subject2</td>
-            <td>High</td>
-            <td>In-Progress</td>		
-            <td>Date</td>
-            <td>Date</td>
-        </tr>
-    </table>
-</body>
+    <body>
+        <?php include 'Header.php'; ?>
+        <?php include 'MenuBar.php'; ?>
+        <br>
+        <br>
+        <table>
+            <tr>
+                <th>Checkbox</th>
+                <th>Ticket#</th>
+                <th>Tickets</th>		
+                <th>Subjects</th>
+                <th>Priority</th>
+                <th>Status</th>		
+                <th>Created</th>
+                <th>Last Updated</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td>1234</td>
+                <td>Ticket1</td>		
+                <td>Subject1</td>
+                <td>Low</td>
+                <td>New</td>		
+                <td>Date</td>
+                <td>Date</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>5678</td>
+                <td>Ticket2</td>		
+                <td>Subject2</td>
+                <td>High</td>
+                <td>In-Progress</td>		
+                <td>Date</td>
+                <td>Date</td>
+            </tr>
+        </table>
+        <br>
+        <br>
+        
+        <div style="text-align: center">
+            <input type=button class="button" onClick="location.href='AddTicket.php'" value='Add Ticket'>
+        </div>
+    </body>
 </html>
--- Welcome.php
+++ Welcome.php
@@ -1,41 +1,15 @@
 <html lang="en"> <!--<![endif]-->
-<head>
-<meta charset="utf-8">
-<title>PKI-STRATOS Welcome</title>
-<link rel="stylesheet" type="text/css" href="style.css"> 
-</head>
+    <head>
+        <meta charset="utf-8">
+        <title>PKI-STRATOS Welcome</title>
+        <link rel="stylesheet" type="text/css" href="style.css"> 
+    </head>
 
-<body>
-    <img src="Header.jpg" alt="Header Image" height="80" style="width:100%">
+    <body>
+        <?php include 'Header.php'; ?>
+        <?php include 'MenuBar.php'; ?>
 
-    <div id='cssmenu'>
-
-        <ul class="menu">
-            <li>
-                <a href='Welcome.php'><span>Home</span></a>
-            </li>
-            <li>
-                <a href="Tickets.php">Tickets</a>
-            </li>
-            <li class="has-sub"><a href=""><span>Inventory</span></a>
-                <ul> 
-                    <li><a href=""><span>Hardware</span></a></li>
-                    <li><a href=""><span>Software</span></a></li>
-                </ul>
-            </li>
-            <li class="has-sub"><a href=""><span>Analytics</span></a>
-                <ul> 
-                    <li><a href=""><span>Metrics</span></a></li>
-                    <li><a href=""><span>Configuration</span></a></li>
-                </ul>
-            </li>
-            <li class="has-sub"><a href=""><span>Accounts</span></a>
-                <ul> 
-                    <li><a href=""><span>Account Settings</span></a></li>
-                    <li><a href=""><span>Logout</span></a></li>
-                </ul>
-            </li>
-    </div>
-    <h1>Welcome to PKI - STRATOS System.</h1>
-</body>
+        <br>
+        <h1 class="alignright">Welcome to PKI - STRATOS System.</h1>
+    </body>
 </html>
