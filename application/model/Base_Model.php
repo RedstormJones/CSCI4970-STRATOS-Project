3f6bd88e61d0037af657a378346cad611b01d336
jvosik <jacob.vosik@gmail.com>
2015-04-06 19:27:23 -0500

    Added generic function for prikeys
    Added GetAndUpdateNextKey( $table ) that queries the primary key table for the next primary key for the given table, then increments it.

--- application/model/Base_Model.php
+++ application/model/Base_Model.php
@@ -7,8 +7,36 @@ class Base_Model
 	public function __construct()
 	{
 		$this->db = DBconnect::getInstance();
+
+		$this->sql_GetNextKey = "
+			SELECT
+				`key`
+			FROM 
+				`StNxtPriKeyInst`
+			WHERE
+				`table` = :table;";
+		$this->query_GetNextKey = $this->db->prepare($this->sql_GetNextKey);
+
+		$this->sql_SetNextKey = "
+			UPDATE
+				`StNxtPriKeyInst`
+			SET
+				`key` = :key
+			WHERE
+				`table` = :table;";
+		$this->query_SetNextKey = $this->db->prepare($this->sql_SetNextKey);
 	}
 
+	public function GetAndUpdateNextKey( $table )
+	{
+		$this->query_GetNextKey->execute( array( ':table' => $table ) );
+		$result = $this->query_GetNextKey->fetchAll();
+
+		$key = $result[0]->key;
+
+		$this->query_SetNextKey->execute( array( ':table' => $table, ':key' => ($key+1) ) );
+		return $key;
+	}
 }
 
 ?>
