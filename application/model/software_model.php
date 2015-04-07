ecf42e9102945826b77b87f4d0f233336627063c
jvosik <jacob.vosik@gmail.com>
2015-04-07 18:51:56 -0500

    Create software_model.php

---
+++ application/model/software_model.php
@@ -0,0 +1,29 @@
+<?php
+include "Base_Model.php";
+
+class Software_Model Extends Base_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->sql_ShowAllSoftware = "
+				SELECT
+                    `sid`
+                  , `name`
+                  , `last_mdfd_user`
+                  , `last_mdfd_tmst`
+                FROM
+                    `StSftInst`
+                WHERE
+                    `logl_del` = FALSE;";
+        $this->query_ShowAllSoftware = $this->db->prepare($this->sql_ShowAllSoftware);
+    }
+
+    public function showAllSoftware()
+    {
+        $this->query_ShowAllSoftware->execute();
+        return $this->query_ShowAllSoftware->fetchAll();
+    }
+}
+
+?>
