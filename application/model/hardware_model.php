ba97a72382819e157bb6fcff6c63a12495bcf002
jvosik <jacob.vosik@gmail.com>
2015-04-07 18:51:26 -0500

    Create hardware_model.php

---
+++ application/model/hardware_model.php
@@ -0,0 +1,34 @@
+<?php
+include "Base_Model.php";
+
+class Hardware_Model Extends Base_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->sql_ShowAllHardware = "
+				SELECT
+                    `eid`
+                  , `name`
+                  , `vendor`
+                  , `model`
+                  , `serial`
+                  , `type`
+                  , `loc`
+                  , `status`
+                  , `last_mdfd_tmst`
+                FROM
+                    `StEqpInst`
+                WHERE
+                    logl_del = FALSE;";
+        $this->query_ShowAllHardware = $this->db->prepare($this->sql_ShowAllHardware);
+    }
+
+    public function showAllHardware()
+    {
+        $this->query_ShowAllHardware->execute();
+        return $this->query_ShowAllHardware->fetchAll();
+    }
+}
+
+?>
