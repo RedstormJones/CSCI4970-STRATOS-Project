715cd4d87444563b1b1c9a80e6014682fbecf46a
jvosik <jacob.vosik@gmail.com>
2015-04-16 17:49:27 -0500

    Create ref_config_base_controller.php

---
+++ application/controller/config/ref_config_base_controller.php
@@ -0,0 +1,16 @@
+<?php
+require_once('../../../globals.php');
+require APP . 'controller\Base_Controller.php';
+
+class Ref_Config_Base_Controller Extends Base_Controller
+{
+   	public function Reassign_and_Delete()
+    {
+        $original = getParam( 'original' , null );
+        $reassign = getParam( 'reassign' , null );
+        $this->model->reassignAndDelete( $original, $reassign );
+        $this->noAction();
+    }
+}
+
+?>
