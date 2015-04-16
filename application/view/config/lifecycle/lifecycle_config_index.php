46d444a38ce2fd59a886179dd2a51616c8541ade
jvosik <jacob.vosik@gmail.com>
2015-04-16 17:45:54 -0500

    Create lifecycle_config_index.php

---
+++ application/view/config/lifecycle/lifecycle_config_index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('../../../globals.php');
+    require APP . 'model\config\lifecycle\lifecycle_config_model.php';
+    require APP . 'controller\config\lifecycle\lifecycle_config_controller.php';
+    require APP . 'view\config\lifecycle\lifecycle_config_view.php';
+
+    $lmodel = new Lifecycle_Config_Model();
+    $lview = new Lifecycle_Config_View();
+    $lcontr = new Lifecycle_Config_Controller($lmodel, $lview);
+    
+    handleURL($lcontr);
+?>
