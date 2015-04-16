a037a5cb45fcd89ef6a83e83205a07fdd2de0da6
jvosik <jacob.vosik@gmail.com>
2015-04-16 17:44:15 -0500

    Reworked elements, added Configuration

--- application/view/_templates/MenuBar.php
+++ application/view/_templates/MenuBar.php
@@ -14,13 +14,16 @@ echo '<div id="cssmenu">
                     <li><a href="/application/view/software/software_index.php"><span>Software</span></a></li>
                 </ul>
             </li>
-            <li class="has-sub"><a href=""><span>Analytics</span></a>
+            <li>
+                <a href="/application/view/metrics/metrics_index.php"><span>Metrics</span></a>
+            </li>
+            <li class="has-sub"><a href=""><span>Configuration</span></a>
                 <ul> 
-                    <li><a href="/application/view/metrics/metrics_index.php"><span>Metrics</span></a></li>
-                    <li><a href=""><span>Configuration</span></a></li>
+                    <li>
+                    <li><a href="/application/view/config/lifecycle/lifecycle_config_index.php"><span>Lifecycle</span></a></li>
                 </ul>
             </li>
-            <li class="has-sub"><a href=""><span>Accounts</span></a>
+            <li class="has-sub"><a href=""><span>Account</span></a>
                 <ul> 
                     <li><a href=""><span>Account Settings</span></a></li>
                     <li><a href=""><span>Logout</span></a></li>
