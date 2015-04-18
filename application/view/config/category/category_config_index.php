0b18bb8d27b8679e451fe1c5fa7d2f6cbfa1c823
jvosik <jacob.vosik@gmail.com>
2015-04-18 14:13:54 -0500

    Create category_config_index.php

---
+++ application/view/config/category/category_config_index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('../../../globals.php');
+    require APP . 'model\config\category\category_config_model.php';
+    require APP . 'controller\config\category\category_config_controller.php';
+    require APP . 'view\config\category\category_config_view.php';
+
+    $model = new Category_Config_Model();
+    $view  = new Category_Config_View();
+    $contr = new Category_Config_Controller($model, $view);
+    
+    handleURL($contr);
+?>
