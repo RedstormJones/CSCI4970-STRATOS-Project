f587d0d426bb6bbc9c9723649685cc4d9cda256a
jvosik <jacob.vosik@gmail.com>
2015-04-18 14:11:34 -0500

    Create priority_config_model.php

---
+++ application/model/config/priority/priority_config_model.php
@@ -0,0 +1,19 @@
+<?php
+require_once('../../../globals.php');
+require APP . 'model\config\Ref_Config_Base_Model.php';
+
+class Priority_Config_Model Extends Ref_Config_Base_Model
+{
+    public function __construct( )
+    {
+        parent::__construct( 'StPriConf'
+                           , 'priority'
+                           , 'name'
+                           , array(
+                                    array( 'StPriMtxConf', 'priority' )
+                                  )
+                           );
+    }
+}
+
+?>
