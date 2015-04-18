2704b710db0cf113a73362ac55f835d88a1fbdd9
jvosik <jacob.vosik@gmail.com>
2015-04-18 14:13:24 -0500

    Create affected_config_view.php

---
+++ application/view/config/affected/affected_config_view.php
@@ -0,0 +1,13 @@
+<?php
+require_once('../../../globals.php');
+require APP . 'view\config\Ref_Config_Base_View.php';
+
+class Affected_Config_View Extends Ref_Config_Base_View
+{
+    public function renderForm( $recordList )
+    {
+        $this->renderBaseForm( $recordList, '' );
+    }
+}
+
+?>
