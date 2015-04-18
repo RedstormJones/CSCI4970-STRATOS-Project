7bc6c2c65af2795c83d676650dc5f9beb9757784
jvosik <jacob.vosik@gmail.com>
2015-04-18 14:15:05 -0500

    Create priority_config_index.php

---
+++ application/view/config/priority/priority_config_index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('../../../globals.php');
+    require APP . 'model\config\priority\priority_config_model.php';
+    require APP . 'controller\config\priority\priority_config_controller.php';
+    require APP . 'view\config\priority\priority_config_view.php';
+
+    $model = new Priority_Config_Model();
+    $view  = new Priority_Config_View();
+    $contr = new Priority_Config_Controller($model, $view);
+    
+    handleURL($contr);
+?>
