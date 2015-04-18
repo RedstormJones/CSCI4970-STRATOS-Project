a8d194b0dc413f81fc6ca97096ed72f26f22c5b5
jvosik <jacob.vosik@gmail.com>
2015-04-18 12:46:42 -0500

    Updated form queries

--- application/controller/tickets/tickets_controller.php
+++ application/controller/tickets/tickets_controller.php
@@ -9,23 +9,23 @@ class Tickets_Controller Extends Base_Controller
         $this->showAllTickets( 0 );
     }
 
-	public function showAllTickets( $start )
-	{
-		$ticket_objects = $this->model->showAllTickets($start);
-		$rows = array();
-		foreach( $ticket_objects as $ticket )
-		{
-			$tid			= isset($ticket->tid) ? $ticket->tid : "";
-			$title			= isset($ticket->title) ? $ticket->title : "";
-			$cname			= isset($ticket->cname) ? $ticket->cname : "";
-			$pname			= isset($ticket->pname) ? $ticket->pname : "";
-			$lname			= isset($ticket->lname) ? $ticket->lname : "";
-			$insrt_tmst		= isset($ticket->insrt_tmst) ? $ticket->insrt_tmst : "";
-			$last_mdfd_tmst = isset($ticket->last_mdfd_tmst) ? $ticket->last_mdfd_tmst : "";
-			$rows[] = array( $tid, $title, $cname, $pname, $lname, $insrt_tmst, $last_mdfd_tmst );
-		}
-		$this->view->renderTickets($rows, $start);
-	}
+    public function showAllTickets( $start )
+    {
+        $ticket_objects = $this->model->showAllTickets($start);
+        $rows = array();
+        foreach( $ticket_objects as $ticket )
+        {
+            $tid                        = isset($ticket->tid) ? $ticket->tid : "";
+            $title                      = isset($ticket->title) ? $ticket->title : "";
+            $cname                      = isset($ticket->cname) ? $ticket->cname : "";
+            $pname                      = isset($ticket->pname) ? $ticket->pname : "";
+            $lname                      = isset($ticket->lname) ? $ticket->lname : "";
+            $insrt_tmst                 = isset($ticket->insrt_tmst) ? $ticket->insrt_tmst : "";
+            $last_mdfd_tmst             = isset($ticket->last_mdfd_tmst) ? $ticket->last_mdfd_tmst : "";
+            $rows[]                     = array( $tid, $title, $cname, $pname, $lname, $insrt_tmst, $last_mdfd_tmst );
+        }
+        $this->view->renderTickets($rows, $start);
+    }
 
     public function Next()
     {
@@ -46,18 +46,57 @@ class Tickets_Controller Extends Base_Controller
         $this->showAllTickets( $start );
     }
 
-	public function Add_Ticket()
-	{
-		$cust_menu = $this->model->getMenu("stprsninst");
-		$assign_menu = $this->model->getMenu("stuserinst");
-		$categ_menu = $this->model->getMenu("stcatgconf");
-		$aff_menu = $this->model->getMenu("stafflvlconf");
-		$sev_menu = $this->model->getMenu("stsvrlvlconf");
+    public function Add_Ticket()
+    {
+        $personsResult                  = $this->model->getAllPersons();
+        $persons                        = array();
+        $users                          = array();
+        foreach( $personsResult as $person )
+        {
+            $pid                        = $person->pid;
+            $fname                      = isset($person->fname) ? $person->fname : "";
+            $lname                      = isset($person->lname) ? $person->lname : "";
+            $user                       = isset($person->user) ? $person->user : null;
 
-		$this->view->renderForm($cust_menu, $assign_menu, $categ_menu, $aff_menu, $sev_menu);
-	}
+            $name                       = $fname . ($fname == "" ? "" : " ") . $lname;
 
-	public function validate_input($data)
+            $resultingPerson            = array( $pid, $name );
+            $persons[]                  = $resultingPerson;
+            if ( $user != null ) 
+                $users[]                = $resultingPerson;
+        }
+    
+        $categoriesResult               = $this->model->getAllCategories();
+        $categories                     = array();
+        foreach( $categoriesResult as $category )
+        {
+            $cid                        = $category->cid;
+            $name                       = $category->name;
+            $categories[]               = array( $cid, $name );
+        }
+		
+        $affectedLevelsResult           = $this->model->getAllAffectedLevels();
+        $affectedLevels                 = array();
+        foreach( $affectedLevelsResult as $affectedLevel )
+        {
+            $aff_level                  = $affectedLevel->aff_level;
+            $name                       = $affectedLevel->name;
+            $affectedLevels[]           = array( $aff_level, $name );
+        }
+        
+        $severityLevelResult            = $this->model->getAllSeverityLevels();
+        $severityLevels                 = array();
+        foreach( $severityLevelResult as $severityLevel )
+        {
+            $severity                   = $severityLevel->severity;
+            $name                       = $severityLevel->name;
+            $severityLevels[]           = array( $severity, $name );
+        }
+
+        $this->view->renderForm($persons, $users, $categories, $affectedLevels, $severityLevels);
+    }
+
+    public function validate_input($data)
     {
     	if(!$data)
     	{
@@ -69,8 +108,8 @@ class Tickets_Controller Extends Base_Controller
         return $data;
     }
 
-	public function validateTicket()
-	{
+    public function validateTicket()
+    {
        if ($_SERVER["REQUEST_METHOD"] == "POST") 
         {
             $title = $this->validate_input($_POST["title"]);
@@ -97,7 +136,7 @@ class Tickets_Controller Extends Base_Controller
                 </script>
             <?php
         }
-	}
+    }
 }
 
 ?>
