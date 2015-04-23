c85d32931cb9728684cf73b4d0afa1c4ed84e4f2
jvosik <jacob.vosik@gmail.com>
2015-04-22 19:07:14 -0500

    Added update and add functions

--- application/controller/config/ref_config_base_controller.php
+++ application/controller/config/ref_config_base_controller.php
@@ -4,13 +4,23 @@ require APP . 'controller\Base_Controller.php';
 
 class Ref_Config_Base_Controller Extends Base_Controller
 {
-   	public function Reassign_and_Delete()
-    {
-        $original = getParam( 'original' , null );
-        $reassign = getParam( 'reassign' , null );
-        $this->model->reassignAndDelete( $original, $reassign );
-        $this->noAction();
-    }
+   public function Reassign_and_Delete()
+   {
+      $original = getParam( 'original' , null );
+      $reassign = getParam( 'reassign' , null );
+      $this->model->reassignAndDelete( $original, $reassign );
+      $this->noAction();
+   }
+   
+   public function Update()
+   {
+      $this->addOrUpdate( true );
+   }
+
+   public function Add()
+   {
+      $this->addOrUpdate( false );
+   } 
 }
 
 ?>
