f43e74b15963d2790ba06d585e735ce9db0688a2
jvosik <jacob.vosik@gmail.com>
2015-04-22 20:05:10 -0500

    Added add and update functionality

--- application/controller/config/affected/affected_config_controller.php
+++ application/controller/config/affected/affected_config_controller.php
@@ -4,18 +4,40 @@ require APP . 'controller\config\Ref_Config_Base_Controller.php';
 
 class Affected_Config_Controller Extends Ref_Config_Base_Controller
 {
-   	public function noAction()
-    {
-        $resultList = $this->model->queryForm();
-        $formElements = array();
-        foreach( $resultList as $result )
-        {
-            $id = isset($result->aff_level) ? $result->aff_level : "";
-            $name = isset($result->name) ? $result->name : "";
-            $formElements[] = array($id,$name);
-        }
-        $this->view->renderForm( $formElements );
-    }
+   public function noAction()
+   {
+      $resultList = $this->model->queryForm();
+      $formElements = array();
+      foreach( $resultList as $result )
+      {
+         $id = isset($result->aff_level) ? $result->aff_level : "";
+         $name = isset($result->name) ? $result->name : "";
+         $formElements[] = array($id,$name);
+      }
+      $this->view->renderForm( $formElements );
+   }
+   
+   public function Add_Affected()
+   {
+      $name = getParam( 'name', null );
+      $this->model->addAffected( $name );
+      $this->startFresh();
+   }
+
+   public function Update_Affected()
+   {
+      $aff_level = getParam( 'aff_level', null );
+      $name = getParam( 'name', null );
+      $this->model->updateAffected( $aff_level, $name );
+      $this->startFresh();
+   }
+
+   public function addOrUpdate( $isUpdate )
+   {
+      $aff_level = getParam( 'original' );
+      $name = $this->model->getAffected( $aff_level )->name;
+      $this->view->renderAddOrUpdate( $isUpdate, $aff_level, $name );
+   }
 }
 
 ?>
