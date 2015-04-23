c66147f3ba21ac2c4b8b0ebe7732b33ac40aa935
jvosik <jacob.vosik@gmail.com>
2015-04-22 19:12:06 -0500

    Added renderAddOrUpdate

--- application/view/config/affected/affected_config_view.php
+++ application/view/config/affected/affected_config_view.php
@@ -14,7 +14,24 @@ class Affected_Config_View Extends Ref_Config_Base_View
                         1. Select the old option in the options list displayed<br>
                         2. Find and select the new option in the drop down list<br>
                         3. Click Reassign and Delete";
-        $this->renderBaseForm( $recordList, $description, '' );
+        $this->renderBaseForm( $recordList, $description );
+    }
+    
+    public function renderAddOrUpdate( $isUpdate, $aff_level, $name )
+    {
+        $body = "<br><br><br>"; 
+        $body .= '<form id="Add" name="AddAffected" method="post" class="dark-matter" action="affected_config_index.php">
+                    <h1>Affected Level ' . ($isUpdate ? 'Updating' : 'Adding') . ' Form
+                        <span>Please fill all the fields.</span>
+                    </h1>
+                    <p>
+                    <label for="textfield">Name:</label>
+                    <input type="text" required="" placeholder="Enter Name" name="name" id="title" value="' . $name . '">
+                    ' . ($isUpdate ? '<input type="hidden" name="aff_level" value="' . $aff_level . '">' : '') . '
+                    <input type="submit" style="margin-left: 35%" class="button" name="action" value="'. ($isUpdate ? 'Update' : 'Add') . ' Affected">
+                    </labelc>
+                </form>';
+        $this->renderBody( $body );
     }
 }
 
