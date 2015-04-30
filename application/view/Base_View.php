ffebc3fde46d8c425b0f0f264a2f51c38d6a3a22
TCF-30 <tfilkins@unomaha.edu>
2015-04-29 22:55:32 -0500

    Updated renderBody() in the Base_View

--- application/view/Base_View.php
+++ application/view/Base_View.php
@@ -9,27 +9,28 @@ class Base_View
 
 	public function renderBody($body)
 	{
-        $head  = '<html>';
-        $head .= '  <head>';
-        $head .= '      <meta charset="utf-8"/>';
-        $head .= '      <title>PKI - STRATOS</title>';
-        $head .= '      <link rel="stylesheet" type="text/css" href="/public/css/style.css" />';
-        $head .= '  </head>';
-        $head .= '  <body>';
-        $head .= '      <div id="header">';
-        $head .= '          <img src="/public/imgs/Header.jpg" alt="Header" width=100% height=7%>';
-        $head .= '      </div>';
-        $head .= '      <div id="bottom">';
-        $head .= '          <div id="menu">';
-
-        $tail  = '              <input type="text" src="/application/view/_templates/MenuBar.php">';
-        $tail .= '          </div>';
-        $tail .= '          <div id="body" class="pagebody">';
+        $head = '<html>
+                    <head>
+                        <meta charset="utf-8"/>
+                        <title>PKI-STRATOS</title>
+                        <link rel="stylesheet" type="text/css" href="/public/css/style.css"/>
+                    </head>
+                    <body>
+                        <div id="header">
+                            <img src="/public/imgs/Header.jpg" alt="Header" width=100% height=7%>
+                        </div>
+                        <div id="bottom">
+                            <div id="menu">';
+                            # MenuBar.php gets inserted here
+        $tail =                 '<input type="text" src="/application/view/_templates/MenuBar.php">
+                            </div>
+                            <div id="body" class="pagebody">';
         $tail .=                $body;
-        $tail .= '          </div>';
-        $tail .= '      </div>';
-        $tail .= '  </body>';
-        $tail .= '</html>';
+        $tail .=            '</div>
+                        </div>
+                    </body>
+                </html>';
+
 
         echo $head;
             require_once(APP . 'view\_templates\MenuBar.php');
